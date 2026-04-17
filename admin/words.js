ccd3f29d76b2207a993b43a9022f2768eb48a9d3
bjoernsto <bjoernstolleld@gmail.com>
2026-04-17 10:16:07 +0200

    Fix: Admin UI and jsonConfig initialization

---
+++ admin/words.js
@@ -0,0 +1,13 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "camera-snapshot adapter settings": {
+        "en": "Adapter settings for camera-snapshot",
+        "de": "Adaptereinstellungen für camera-snapshot"
+    },
+    "Please use a modern Admin adapter to configure this adapter via jsonConfig.": {
+        "en": "Please use a modern Admin adapter to configure this adapter via jsonConfig.",
+        "de": "Bitte verwende einen aktuellen Admin-Adapter, um diesen Adapter über jsonConfig zu konfigurieren."
+    }
+};
